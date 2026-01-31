34a58574126998f0de11d625d3cb5b754b3424ce
ChuckySpider <chucky96911@gmail.com>
2026-01-31 18:10:26 +0000

    dog completed

---
+++ mydog.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(100, 50, 220); //red,green,blue
+  
+  fill(50,50,50);
+  circle(180, 230, 100);
+  
+  fill(50,50,50);
+  ellipse(240,200,70,40);
+  
+  fill(50,50,50);
+  ellipse(250,180,20,35);
+  
+  fill(200);
+  circle(250,195,10,10);
+  
+  rect(200,270,10,30);
+  rect(150,270,10,30);
+  rect(100,220, 30,10);
+  
+  fill(0,200,0);
+  rect(0,300,400,100);
+}
